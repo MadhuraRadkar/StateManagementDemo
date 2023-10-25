64fc827d22b4b793d691986f1dba62a389fe745c
MadhuraRadkar <madhuraradkar@gmail.com>
2023-10-25 22:26:25 +0530

    Add project files.

---
+++ StateManagementDemo/Controllers/ReadCookieSessionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StateManagementDemo.Controllers
+{
+    public class ReadCookieSessionController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ReadCookieSessionController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult ReadCookie()
+        {
+            if(_httpContextAccessor.HttpContext != null)
+            {
+                // Read value from cookie.
+                ViewBag.Email = _httpContextAccessor.HttpContext.Request.Cookies["email"];
+            }
+            return View();
+        }
+
+        public IActionResult ReadSession()
+        {
+            ViewBag.Email = HttpContext.Session.GetString("email");
+            //string role=HttpContext.Session.GetString("roleId");
+            return View();
+        }
+
+    }
+}
